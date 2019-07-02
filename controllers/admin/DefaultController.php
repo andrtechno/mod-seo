21a270b10242f6eb2cf5eb9053a5ca525d3b3872
Andrew <andrew.panix@gmail.com>
2019-07-02 18:07:33 +0300

    init

--- controllers/admin/DefaultController.php
+++ controllers/admin/DefaultController.php
@@ -93,6 +93,7 @@ class DefaultController extends AdminController
         $model = SeoUrl::findModel($id);
         $this->pageName = Yii::t('app', 'UPDATE', 0);
         $post = Yii::$app->request->post();
+        $isNew = $model->isNewRecord;
         if ($model->load($post)) {
             /* update url */
             if ($model->validate()) {
@@ -120,6 +121,7 @@ class DefaultController extends AdminController
 
                     $this->saveParams($model);*/
 
+                    Yii::$app->session->setFlash('success', Yii::t('app', ($isNew) ? 'SUCCESS_CREATE' : 'SUCCESS_UPDATE'));
                     $redirect = (isset($post['redirect'])) ? $post['redirect'] : Yii::$app->request->url;
                     if (!Yii::$app->request->isAjax)
                         return $this->redirect($redirect);
