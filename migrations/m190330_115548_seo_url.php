1d6fb0d6d901bdef0d90806293cd23387891585b
Andrew <andrew.panix@gmail.com>
2019-10-02 17:42:20 +0300

    init

--- migrations/m190330_115548_seo_url.php
+++ migrations/m190330_115548_seo_url.php
@@ -13,20 +13,19 @@ namespace panix\mod\seo\migrations;
 
 use panix\engine\db\Migration;
 use panix\mod\seo\models\SeoUrl;
-use panix\mod\seo\models\SettingsForm;
-use panix\engine\components\Settings;
 
 class m190330_115548_seo_url extends Migration
 {
 
-    // Use up()/down() to run migration code without a transaction.
+    public $settingsForm = 'panix\mod\seo\models\SettingsForm';
+
     public function up()
     {
         $this->createTable(SeoUrl::tableName(), [
             'id' => $this->primaryKey()->unsigned(),
             'url' => $this->string(255)->notNull(),
             'title' => $this->string(150)->null(),
-            'domain'=>$this->tinyInteger(1)->defaultValue(1),
+            'domain' => $this->tinyInteger(1)->defaultValue(1),
             'description' => $this->string(255)->null(),
             'meta_robots' => $this->string(16)->null(),
             'h1' => $this->string(255)->null(),
@@ -34,15 +33,7 @@ class m190330_115548_seo_url extends Migration
         ], $this->tableOptions);
 
         $this->createIndex('url', SeoUrl::tableName(), 'url');
-
-        $settings = [];
-        foreach (SettingsForm::defaultSettings() as $key => $value) {
-            $settings[] = [SettingsForm::$category, $key, $value];
-        }
-
-        $this->batchInsert(Settings::tableName(), ['category', 'param', 'value'], $settings);
-
-
+        $this->loadSettings();
     }
 
     public function down()
